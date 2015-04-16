9c0a14a912c10a742e585c880c9f861faab5cb2c
CKK-1 <CKKleitsch@gmail.com>
2015-04-16 09:30:16 -0700

    Activity 2 Only Deck
    Tester not complete

--- Activity02/DeckTester.java
+++ Activity02/DeckTester.java
@@ -1,15 +1,44 @@
 package Activity02;
 
-/**
- * This is a class that tests the Deck class.
- */
-public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class DeckTester.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DeckTester
+{
+    /**
+     * Default constructor for test class DeckTester
+     */
+    public DeckTester()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
 }
